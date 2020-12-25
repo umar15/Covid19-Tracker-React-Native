ad29b9bd5f5fbfd0b3afdd01bb51671f1a6733f7
Umar Khalid <umar.qur105@gmail.com>
2020-12-25 20:53:26 +0500

    Global Stats Added

--- Screens/WorldStats.js
+++ Screens/WorldStats.js
@@ -1,14 +1,49 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
 
-export class WorldStats extends Component {
-	render() {
-		return (
-			<View>
-				<Text> World Stats </Text>
-			</View>
+const WorldStats = () => {
+	const [GlobalStats, setGlobalStats] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const getData = async () => {
+		const response = await fetch(
+			"https://covid-19-data.p.rapidapi.com/totals",
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-key":
+						"6855cf7242msh879a563e0d43b0dp12a354jsn5a0fefe10680",
+					"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+				},
+			}
 		);
-	}
-}
+		const data = await response.json();
+		const newData = {
+			confirmed: data[0].confirmed,
+			deaths: data[0].deaths,
+			critical: data[0].critical,
+			recovered: data[0].recovered,
+			lastUpdate: new Date(data[0].lastUpdate).toDateString(),
+		};
+		setGlobalStats(newData);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<View>
+			<Text>World Stats</Text>
+			<View>
+				<Text>Confirmed: {GlobalStats.confirmed}</Text>
+				<Text>deaths: {GlobalStats.deaths}</Text>
+				<Text>critical: {GlobalStats.critical}</Text>
+				<Text>recovered: {GlobalStats.recovered}</Text>
+				<Text>lastUpdate: {GlobalStats.lastUpdate}</Text>
+			</View>
+		</View>
+	);
+};
 
 export default WorldStats;
